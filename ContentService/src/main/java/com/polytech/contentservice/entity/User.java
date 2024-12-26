f0ceb950b8b7fdd3d1b86dd980002c378c7aced9
llav3ji2019 <pavelemelyanov03@mail.ru>
2024-12-26 04:12:50 +0300

    feature(CS): add user repository
    Metadata: OTDELRAZRABOTKI-51

---
+++ ContentService/src/main/java/com/polytech/contentservice/entity/User.java
@@ -0,0 +1,37 @@
+package com.polytech.contentservice.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue
+    @UuidGenerator
+    @Column(name = "user_id")
+    private UUID id;
+    private String login;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    private String password;
+    private String email;
+    private String phone;
+    private LocalDate birthday;
+    @Column(name = "creation_date")
+    private LocalDateTime creationDate;
+    @Column(name = "updated_date")
+    private LocalDateTime updateDate;
+}
