59c9657fcdeb573963435231ffd3053b37636784
llav3ji2019 <pavelemelyanov03@mail.ru>
2025-01-04 02:33:31 +0300

    feature(CS): Update ContentGrpcController
    Metadata: OTDELRAZRABOTKI-72

--- ContentService/src/main/java/com/polytech/contentservice/grpc/ContentGrpcController.java
+++ ContentService/src/main/java/com/polytech/contentservice/grpc/ContentGrpcController.java
@@ -2,7 +2,11 @@ package com.polytech.contentservice.grpc;
 
 import com.google.rpc.Code;
 import com.google.rpc.Status;
+import com.polytech.contentservice.dto.content.ContentDto;
+import com.polytech.contentservice.dto.episode.EpisodeDto;
 import com.polytech.contentservice.dto.user.detailed.UserDto;
+import com.polytech.contentservice.service.content.ContentService;
+import com.polytech.contentservice.service.episode.EpisodeService;
 import com.polytech.contentservice.service.user.UserService;
 import content.ContentServiceGrpc;
 import content.UserRoleRequest;
@@ -10,6 +14,7 @@ import content.UserRoleResponse;
 import io.grpc.protobuf.StatusProto;
 import io.grpc.stub.StreamObserver;
 import java.util.Optional;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.lognet.springboot.grpc.GRpcService;
 
@@ -17,6 +22,8 @@ import org.lognet.springboot.grpc.GRpcService;
 @RequiredArgsConstructor
 public class ContentGrpcController extends ContentServiceGrpc.ContentServiceImplBase {
   private final UserService userService;
+  private final EpisodeService episodeService;
+  private final ContentService contentService;
 
   @Override
   public void getUserRole(UserRoleRequest userRoleRequest,
@@ -36,5 +43,25 @@ public class ContentGrpcController extends ContentServiceGrpc.ContentServiceImpl
             .build()
     );
     responseObserver.onCompleted();
+  }
+
+  @Override
+  public void setContentImageUrl(content.SetContentImageUrlRequest request,
+                                 io.grpc.stub.StreamObserver<com.google.protobuf.Empty> responseObserver) {
+    contentService.updateContent(UUID.fromString(request.getContentId()),
+        ContentDto.builder()
+            .thumbnail(request.getUrl())
+            .build());
+    responseObserver.onNext(com.google.protobuf.Empty.getDefaultInstance());
+  }
+
+  @Override
+  public void setEpisodeVideoUrl(content.SetEpisodeVideoUrlRequest request,
+                                 io.grpc.stub.StreamObserver<com.google.protobuf.Empty> responseObserver) {
+    episodeService.updateEpisodeInfo(UUID.fromString(request.getEpisodeId()),
+        EpisodeDto.builder()
+            .s3BucketName(request.getUrl())
+            .build());
+    responseObserver.onNext(com.google.protobuf.Empty.getDefaultInstance());
   }
 }
